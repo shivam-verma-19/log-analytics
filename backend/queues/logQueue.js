dbdfe631fa463cfde7449219d5f5c479959664ac
sourav.shivam1998@gmail.com <sourav.shivam1998@gmail.com>
2025-03-16 14:08:14 +0530

    Refactor logQueue to use client from redis configuration module

--- backend/queues/logQueue.js
+++ backend/queues/logQueue.js
@@ -1,8 +1,8 @@
 import { Queue } from 'bullmq';
-import redis from '../config/redisConfig.js';
+import { client } from '../config/redisConfig.js';
 
 export const logQueue = new Queue('log-processing-queue', {
-    connection: redis,
+    connection: client,
     defaultJobOptions: {
         attempts: 3, // Retry failed jobs 3 times
         priority: 1, // Lower number = higher priority
